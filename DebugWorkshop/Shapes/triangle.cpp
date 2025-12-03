a8a4c0e83c84afc0ad609442b4ede7433057f35f
enderjane <idonevet@gmail.com>
2025-12-03 18:47:57 +0200

    Fix: fixed bug in Shapes folder

--- DebugWorkshop/Shapes/triangle.cpp
+++ DebugWorkshop/Shapes/triangle.cpp
@@ -6,12 +6,13 @@ Triangle::Triangle(float base, float height) : _base(base), _height(height)
 {
 }
 
-float Triangle::get_area(bool has_depth) const
+float Triangle::get_area() const //had bool has_depth in the parameters which was different from the definition in shape.h
 {
-	if (has_depth)
-	{
-		throw std::exception("3D triangle is not implemented yet!");
-	}
+	//if (bool has_depth)
+	//{
+	//	throw std::exception("3D triangle is not implemented yet!");
+	//}
+	//unneccessary because it is never used
 
 	return float(0.5 * _base * _height);
 }
